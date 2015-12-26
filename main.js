c20b6f3464cd2a844ff021ed6d968c24d2657d69
Amir Torrez <johnamtorres@gmail.com>
2015-12-26 17:43:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function html(var vb,vl){
-  document.getElementById(vb).innerHTML = vl;
-};
+var gID,iVL,aNM;
+
+function byId (gID){byID = document.getElementById(gID);return byID;};
+function html (gID,iVL){byId(gID).innerHTML = iVL;};
+function attr (gID,aNM,iVL){byId(gID).setAttribute(aNM, iVL);};
+function hide (gID){byId(gID).setAttribute("style", "display:none;");};
+function show (gID){byId(gID).setAttribute("style", "display:initial;");};
+
 window.onload = function() {
-  document.getElementById("main").innerHTML = "¡Hola mundo!";
+  html("main","khe");
+  html("main1","kha");
 };
